94370aba99cf6065f71a3a158bbcd9bd45d49106
Prithvi Jamadagni <52258420+Pritjam@users.noreply.github.com>
2022-09-18 14:22:30 -0500

    add all website files from website repo

---
+++ php/guestbookadd.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<!-- TODO: a js script that sanitizes inputs? -->
+
+    <?php
+        $book = fopen("guestbook.txt", "a") or die("Unable to open guestbook for appending!");
+        $name = $_POST["name"];
+        fwrite($book, date("Y-m-d", time()));
+
+        fwrite($book, "\t");
+        fwrite($book, $name);
+        fwrite($book, "\t");
+        $message = $_POST["message"];
+        fwrite($book, $message);
+        fwrite($book, "\n");
+        fclose($book);
+
+        header("Location: guestbookview.php");
+        exit();
+    ?>
+
+</body>
+
+</html>
---
+++ php/guestbookview.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Pritjam</title>
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/guestbook.css">
+</head>
+
+<body>
+	<div class="header">
+		<h1>Prithvi Jamadagni's Webpage</h1>
+		<p class="motd">Howdy, and welcome to my website!</p>
+	</div>
+
+	<!-- Site navigation menu -->
+	<ul class="navbar">
+		<li><a href="../index.html">Home</a></li>
+		<li><a href="../games.html">My Games</a></li>
+		<li><a href="../projects.html">Projects</a></li>
+		<li><a href="../index.html">About Me</a></li>
+	</ul>
+
+	<div class="section">
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Message</th>
+                <th>Timestamp</th>
+            </tr>
+		    <?php
+                $book = fopen("guestbook.txt", "r") or die("Unable to open guestbook for reading!");
+                while(!feof($book)) {
+                    $output = "<tr><td>";
+                    $line = fgetss($book);
+                    $toks = explode("\t", $line);
+                    if(count($toks) != 3) {
+                        continue;
+                    }
+                    $output .= $toks[1];
+                    $output .= "</td><td>";
+                    $output .= $toks[2];
+                    $output .= "</td><td>";
+                    $output .= $toks[0];
+                    $output .= "</td></tr>";
+                    echo $output;
+                }
+                fclose($book);
+            ?>
+        </table>
+	</div>
+</body>
+
+</html>
